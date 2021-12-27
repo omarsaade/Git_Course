d058c5535692198d186e7f5dd5d41f0f4677132f
omar saade <omarsaadeh11@gmail.com>
2021-12-28 00:25:40 +0200

    git learning

--- js/G.js
+++ js/G.js
@@ -1,16 +1,20 @@
-// repositry : mustawda3 el code , 
+//3mol github account
+// download git aw cmder(full)
+
+
+// repositry : mustawda3 el code ,
 // --i5tisara repo w kell project fi 3ando repo.
 //ex: cocacolo maser
 
 
 //-Branch yaane fare3 aw kesem
-// ex:cairo branch 
+// ex:cairo branch
 
 //local repo yaane pc taba3ak
 //remote repo yaane github
 
 //Commit : bas t7ell bug aw ta3mul(snapshot or checkpoint in ur local repo)
-// shi lezem ta3mul degre baada commit , hye btkun lessa ma nba3tet wala hassala shi 
+// shi lezem ta3mul degre baada commit , hye btkun lessa ma nba3tet wala hassala shi
 // bas gayarna w 3adalna
 
 
@@ -26,7 +30,7 @@
 
 
 //STep by STep
-//1_mnfta7 el Git Bash ma7al ma badna ne5la2 el Folder li badna 
+//1_mnfta7 el Git Bash ma7al ma badna ne5la2 el Folder li badna
 //na3mul fi Clone .
 
 //2-mkdir ESEMelFOLDER         ex: mkdir My_Github
@@ -38,13 +42,13 @@
 
 //5-dir        fawetna 3le
 
-//6-cd Git_Course 
+//6-cd Git_Course
 //mnetkob hed cd esem el repo
 
 //7-dir
 //btfawetne 3a readme.md
 
-//8-mne5la2 belll My_Github folder (file.js aw file.html)aw 
+//8-mne5la2 belll My_Github folder (file.js aw file.html)aw
 //aw folder mnsami w mnhet bi albo (file.js aw file.html 3ade..)
 
 
@@ -62,13 +66,24 @@
 
 // note:(iza ana tlla3et file aw folder 3al stage bel galat w habeb shila
 //yaane habeb a3mela unstage bi basata , la ne3teber 3amalna create
-//la file esmo index.css bel folder (my-Github/Git-course) w 3melnelo (git add index.css) 
+//la file esmo index.css bel folder (my-Github/Git-course) w 3melnelo (git add index.css)
 //hon ana bkun tala3to , tayeb ana lezem shilo hala2 men baad ma tala3to ,yaane an3emlo unstage
 // kif? mnektob git resest head index.css byetla3 men el stage
 
 
-//11-git commit -m"text" aw  git commit 
+//11-git commit -m"text" aw  git commit
 //ba3da mnektob text w ba3da ,mnef2os (shift ك )sawa w mnektob wq w ba3den mnetla3
 // sar kelshi bel local repo
 
-//12-
+//12-note
+// git branch
+//bi 3arrfak kell el branches el mawjude 3andak bel local
+
+//kif mnjib remote name ?li ana lah wade 3le? =>>> git remote -v
+
+//The 'U' means the files are 'untracked',
+// and the 'M' means the files have been 'modified'.
+//git add -A - To add all the files to the staging area.
+
+
+
