e591e49c09af62bdfe40750a690aad4b26578164
omar saade <omarsaadeh11@gmail.com>
2021-12-27 23:31:50 +0200

    git learning

---
+++ js/G.js
@@ -0,0 +1,74 @@
+// repositry : mustawda3 el code , 
+// --i5tisara repo w kell project fi 3ando repo.
+//ex: cocacolo maser
+
+
+//-Branch yaane fare3 aw kesem
+// ex:cairo branch 
+
+//local repo yaane pc taba3ak
+//remote repo yaane github
+
+//Commit : bas t7ell bug aw ta3mul(snapshot or checkpoint in ur local repo)
+// shi lezem ta3mul degre baada commit , hye btkun lessa ma nba3tet wala hassala shi 
+// bas gayarna w 3adalna
+
+
+//clone yaane nase5
+//(repo from remote) or (remote from repo)
+
+//push [upload local changes to remote]
+//pull [you pull changes from remote repo to your local]
+//pull request [tell other about your changes to pull it from local to remote]
+
+//pull and pull request fiah reject
+
+
+
+//STep by STep
+//1_mnfta7 el Git Bash ma7al ma badna ne5la2 el Folder li badna 
+//na3mul fi Clone .
+
+//2-mkdir ESEMelFOLDER         ex: mkdir My_Github
+
+//3-cd My_Github              mnfut 3le lal folder
+
+//4-git clone https://github.com/omarsaade/Git_Coure.git
+// mnjib el link hayda men el repo/Code/HTTPS
+
+//5-dir        fawetna 3le
+
+//6-cd Git_Course 
+//mnetkob hed cd esem el repo
+
+//7-dir
+//btfawetne 3a readme.md
+
+//8-mne5la2 belll My_Github folder (file.js aw file.html)aw 
+//aw folder mnsami w mnhet bi albo (file.js aw file.html 3ade..)
+
+
+//9- git status
+//lama nektob hayde bt2elna el 7ale el 3ame,
+//shu sayer hala2 wshu baad lezem yenzed aw bado idafe 3aL STAGE
+
+
+//10-git add .   // aw git add *  (yaane kell el melafet)
+//hayde btzid el folder w btheto bel staging area
+//working Directory -(git add .)> staging area -(git commit)> local repo -->
+//-(git push)> remote repo
+// fina n2ul kamen git add folder\lo.js lo.html
+
+
+// note:(iza ana tlla3et file aw folder 3al stage bel galat w habeb shila
+//yaane habeb a3mela unstage bi basata , la ne3teber 3amalna create
+//la file esmo index.css bel folder (my-Github/Git-course) w 3melnelo (git add index.css) 
+//hon ana bkun tala3to , tayeb ana lezem shilo hala2 men baad ma tala3to ,yaane an3emlo unstage
+// kif? mnektob git resest head index.css byetla3 men el stage
+
+
+//11-git commit -m"text" aw  git commit 
+//ba3da mnektob text w ba3da ,mnef2os (shift ك )sawa w mnektob wq w ba3den mnetla3
+// sar kelshi bel local repo
+
+//12-
