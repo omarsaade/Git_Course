bcb2ebb3e45b34096b44399a61734e85ecb02c19
omar saade <omarsaadeh11@gmail.com>
2021-12-30 00:23:02 +0200

    git Config

--- js/G.js
+++ js/G.js
@@ -27,13 +27,13 @@
 
 //pull and pull request fiah reject
 
-
+//touch ammur.txt     bte5la2 file
 
 //STep by STep
 //1_mnfta7 el Git Bash ma7al ma badna ne5la2 el Folder li badna
 //na3mul fi Clone .
 
-//2-mkdir ESEMelFOLDER         ex: mkdir My_Github
+//2-mkdir ESEMelFOLDER         ex: mkdir My_Github    bte5la2 folder
 
 //3-cd My_Github              mnfut 3le lal folder
 
@@ -85,5 +85,119 @@
 // and the 'M' means the files have been 'modified'.
 //git add -A - To add all the files to the staging area.
 
-//13
+
+
+
+
+
+
+
+
+//??????////
+
+
+
+//////Tadrib la tari2et shegel el team ma3 b3d////
+// new repo from a new github account
+// mnfut 3a repo w mne5od el link men repo par example
+//github.com//elzerocourse/our.project
+//mnjibo w mnefta7 tene account yalle esmo omar w mnhot link jaweto w byefta7
+//hala2 badna ne5od project men omar(person men el team)
+//fork yaanne nes5a men el project for u to commit ur proposed changes to
+//delelte
+
+
+// hala2 ana ka omar 3ayez kun wahad men el team taba3 el project yalle 3amlo el ,34
+// m3alem mudir el sherke par ex soooo kif?
+//bi fut saheb el shereke 3a account el github taba3o w bi fut 3al project
+// settings logo
+//Collaborators
+//bytetla3lo metel search bar , byetktob fia esem eli bado yfut 3a team(mwazaf)
+//add collaborator
+//hon bikun nba3at invitation 3an tari2 el email lal mwazaf , bi fut huwe 3al
+//email w bi ballesh accepto accepto
+//accept invitation
+//w hik sar wahad men el team
+
+// hala2 fina ne5od link taba3 repo
+// git clone HTTPS bel folder
+//mnfut 3le bel cd ourproject
+// w mnna3melo git add . / git commit -m"bla bla" / git push / git status
+
+// w hik bikun nzed el Malaf 3ande w 3ando w 3and kell el team
+//yaane 3al centralize server
+
+
+
+// hala2 iza hab el sha5es el jdid ydif bel projet w bifut 3le w bi na2e
+//create new file
+// bi sammi osama.txt
+//description
+//Commit new file
+// hala2 bi bayen 3nd el call bel remote repo
+//bas baad ma bayan ka folder bel pc..esseh 3al repo
+
+//git status
+//git pull origin
+//he li faw2na bta3mul git fetch ma3 git merge sawa
+//baad ma na3mla bi bayen el osama.txt bel folder
+//TAMEMO
+//GIT STATUS
+// HIK BTSIR TA3REF TECHTEGEL MA3 EL TEAM
+
+
+
+//VIDEO 7
+// LEARN GIT Gonfiguration
+//BUG YAANE 5ALAL BARMAJI
+// touch new_file.txt    bte5la2 new file
+
+
+//1
+//git config  -l
+//btjib list bi kelshi config
+
+//git help congif
+//byefta7 kelshi 5aso bel config
+//fia kelshi khaso bel doc
+
+// n3adel 3al COnfig??kif
+
+//ex
+//useremail
+//  git config --global user.email    (hayde get)
+// value of useremail
+// git config --global user.email "o@gmail.com"  (hayde set)
+
+// b3den  git config --global user.email (btbayenn)
+
+
+
+//username
+// git config --global user.name "osamaelzero"  (hayde set)
+// b3den  git config --global user.name (btbayenn)
+
+
+//git config -l       lanshuf
+//metel git status
+
+
+// git config -l --show-origin
+//bt2elak btjib el config taba3ak men wen , men aya file
+
+// git config --global --unset user.name
+//hayde btshil shi men el config metel , user.name
+
+
+//git config -l
+
+// $$$$$$ $$$$$ 555555
+//git config --global --edit
+//tari2et ta3dil jdide 3al config yalle byefta7 auto el editor
+//w mnzid hawde
+//  [color "status"] added = green  changed = red bold  untracked = magenta bold
+// [color "branch"] remote = yellow
+
+
+
 
