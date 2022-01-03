637037044e35990b5dbc4a541abf5dab6f394e03
omar saade <omarsaadeh11@gmail.com>
2022-01-03 23:44:06 +0200

    command line course

---
+++ js/command.js
@@ -0,0 +1,90 @@
+//Learn Command Line
+
+// shell = command line interpreter : bi tarjjem
+// el shi li mnektbo bel terminal
+
+// terminal =  text input / output environement
+
+// CMD : is the original shell for the Moircrosoft Dos opertaing System
+
+//PowerShell =  after CMD
+// is the best
+// a2wa men cmd bi ktirrrrr
+
+
+// tab             IMP
+// cd..        bterja3 5etwe la wara
+// cd cmder\       btfawtne 3le
+//cd ..
+// cd\         btraje3na 3a root(local disc)
+// mkdir or  md              make directory
+// clear  or cls
+//mkdir css js fonts img assets     5lo2 hal files
+//mkdir css            jawebo  css/
+// ls   list
+//mkdir "Osama elzero"        jawbo flder 'osama Elzero'
+// dir                metel ls
+
+// c:\elzero\css
+//3teber enta habeb ta3mul folder bel elzero mesh
+// bel css , kif bta3mla ?
+// mkdir ..\Testing
+// c:\elzero
+//mkdir c:\React
+//cd React\
+//cd c:\elzero\
+
+//video 4
+// mv shi la shi
+// ex: mv java ruby
+// amer mv bya3mul rename and move at the same time
+// mv Testing ..\       yaane raje3 el Testing la ma7alo
+
+//mv x..
+// yaane la raje3 el x la wad3o
+
+//rename c z     yaane bi sammi el c  => z
+
+//c:/elzero
+//cd..
+//c:\
+
+//cp --help
+//mv --help
+//cp --r  css css2        samie ee css => css2
+
+///rm -r css     byem7e css li bi albo  w huwe
+//rm --help
+//rm --d  Z       bye7zof el fade bas
+
+// video 5
+// cat jeye men concatenate
+// bte3rad mu7atwa malaf
+
+//echo btetba3
+// > >>
+//echo "hello" > osama.txt   iza mn zid bya3mul override
+//echo "my" >> osama.txt          bi zid wara baad
+//cat osama.txt file.txt 3ared mu7atwa
+//cat *
+
+
+// el cat fia ta3mul copy ka ex:
+// cat osama.txt > zero.txt
+
+//cat last.txt osama.txt bro.txt > finaldestination.txt
+//cat finaldestination.txt
+
+
+// video6
+//grep     global regural expression print
+// grep bta3mul bahes bel stur aw bel files aw bel folders
+// 3an kelme aw jelme m3ayane enta bta3ti yeha
+
+
+//grep -r "samir"
+//bet barem 3ala samir bel file
+
+
+//grep -r -l "samir"
+//bet barem 3a list el files li fionn maktub samir 
