420d17ac66f3e58a66f469d816c4db97180bb66b
omar saade <omarsaadeh11@gmail.com>
2022-01-04 20:48:41 +0200

    command line course

--- js/command.js
+++ js/command.js
@@ -87,4 +87,137 @@
 
 
 //grep -r -l "samir"
-//bet barem 3a list el files li fionn maktub samir 
+//bet barem 3a list el files li fionn maktub samir
+
+
+//mkdir osama
+// echo "SD" >> osama/os.txt
+// ansha2na folder esmo osama w hatayna jaweto
+// file esmo os.txt w katabna fi "SD"
+
+
+// grep "XD" -r osama
+// barrem3a xd bi ousama
+
+// importanteeee \
+// grep "XD" C:\osama\ahmed.txt
+
+//aw iza folder wahad
+//grep "XD" -r osama
+// grep "XD" -r  C:\elzero
+
+
+//file osama.txt
+//jaweb : ASCII text
+//bt2elak shu huwe mu7atwe
+
+//osama.docx
+//mircosoft word 2007
+//yaane deyman bya3tik mu7atwee mesh imtidedo
+
+// iza badna na3mul rename la file kif ?
+//rename osama.jpg osama.docx    => sami he la he
+
+//////////////////////////
+//man
+// move /?             manual mhem
+
+//ping elzero.org           network
+//ping elzero.org -t       t bisir ma bye5las
+// byeb3at 5bar w bi redelak iza el mawke3 shegaal aw la2
+// iza mawjud aw la2
+
+
+// ping elzero.org > ping_for_ELzero.txt
+//
+
+
+
+//btshuf kelshi men khilel (btshuf el benye)
+//tree /a
+
+// ex:
+// mkdir js csss fonts
+//cd fonts\
+// md fontawesome fontello glyph
+//cd ..
+//tree /a
+
+//osk
+//he bteftah el screen keyboard
+
+//control alt delete
+//or
+//tasklist
+
+
+//amazing meth to kill a program and close it
+// https://winaero.com/kill-process-windows-10/
+
+
+// open calculator shortcut cmd       calc
+//start chrome
+//code   to open vsc
+// tskill chrome        to close chrome
+
+//tasklist
+
+//taskkill /F /PID pid_number
+//ex: taskkill /F /PID 1242
+
+
+// taskkill /IM "process name" /F
+//ex: taskkill /IM "notepad.exe" /F
+
+//explorer elzero
+// shortcut to open folder
+
+//alias
+// ex
+// alias tasklist = tk
+
+// delete ana alias
+// alias /d tasklist
+
+
+//         alias /?            ma3lument 3n alias
+
+//mspaint   yaane fteh el microsoft paint
+
+// ipconfig && mspaint
+// fik testa5dem amren sawa
+// t2elo yeftah el paint w yjeblak el ipcongif
+
+// whoami
+// bya3tik esem el user
+
+
+// systeminfo
+// info about system kelo
+
+
+// ping elzero.org     bi jeblak ma3lumeto
+//ping elzero.org > s.txt
+// cat s.txt
+
+// clipboard windows yaane lama ana ba3mul copy la text
+// m3ayan huwe bi ruh 3al clipboard
+
+
+// ipconfig | clip
+// hayde bteb3at el ma3lumet li bel ipconfig lal clipboard
+
+// how to open file from terminal?
+
+
+/////////////////////////////
+
+//ss64.com
+// hayda bi albo kell el ma3lumet tab3un el shortcut
+
+
+// to open a website
+//  start chrome http://www.youtube.com
+
+
+// start chrome ss64.com
